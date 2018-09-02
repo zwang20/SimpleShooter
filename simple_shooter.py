090210ce88761dd3805254225971d9b407d0f30b
Michael <zwang20@knox.nsw.edu.au>
2018-09-02 18:52:53 +1000

    Minor change

--- simple_shooter.py
+++ simple_shooter.py
@@ -106,6 +106,7 @@ game_display = pygame.display.set_mode([display_width, display_height])
 # background_image = pygame.image.load(os.path.join('assets', 'background_image.png'))
 # background_image = pygame.transform.scale(background_image, (display_width, display_height))
 
+
 sge_clear()
 sge_print(string='Loading instructions')
 pygame.display.update()
