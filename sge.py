293b35ca3816ef8ca9e6b0d6b89d637d54248a32
Michael <zwang20@knox.nsw.edu.au>
2018-09-06 11:27:21 +1000

    pep 8

--- sge.py
+++ sge.py
@@ -4,11 +4,15 @@ from data import *
 print('Simple Game Engine by Michael')
 
 
-def sge_print(display=game_display, string='Test', x=0, y=0, colour=(0, 0, 0)):
-    display.blit(pygame.font.SysFont("arial",
-        25).render(str(string), True, colour), (x, y))
+def sge_print(
+        display=game_display, string='Test', x=0, y=0, colour=(0, 0, 0)):
+    display.blit(
+        pygame.font.SysFont(
+            "arial", 25).render(str(string), True, colour), (x, y))
 
-def sge_rect(display=game_display, x=0, y=0, width=10, height=10, colour=(0, 0, 0)):
+
+def sge_rect(
+        display=game_display, x=0, y=0, width=10, height=10, colour=(0, 0, 0)):
     if x <= 0:
         x = 1
     if y <= 0:
@@ -25,7 +29,8 @@ def sge_clear(display=game_display, colour=(255, 255, 255)):
 
 
 def sge_line(
-display=game_display, colour=(0,0,0), point_1=(0, 0), point_2=(10, 10), width=(10)
+    display=game_display,
+    colour=(0, 0, 0), point_1=(0, 0), point_2=(10, 10), width=(10)
 ):
     pygame.draw.line(display, colour, point_1, point_2, width)
 
@@ -46,6 +51,7 @@ def sge_load():
         x = open('settings.dat', 'w')
         x.close()
         return None
+
 
 def sge_write():
     pass
